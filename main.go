72d31361b11b4abf1937eb6db9d6b1c52652bbef
hyun2 <hks0284@gmail.com>
2022-03-18 18:07:41 +0900

    feat: How to add a transaction

--- main.go
+++ main.go
@@ -12,41 +12,47 @@ type Block struct {
 	timestamp    int64
 	nonce        int
 	previousHash [32]byte
-	transactions []string
-}
-
-func NewBlock(nonce int, previousHash [32]byte) *Block {
-	b := Block{
-		timestamp:    time.Now().UnixNano(),
-		nonce:        nonce,
-		previousHash: previousHash,
-		transactions: []string{},
-	}
-
-	return &b
+	transactions []*Transaction
 }
 
 // func NewBlock(nonce int, previousHash [32]byte) *Block {
-// 	b := new(Block)
-// 	b.timestamp = time.Now().UnixNano()
-// 	b.nonce = nonce
-// 	b.previousHash = previousHash
-// 	return b
+// 	b := Block{
+// 		timestamp:    time.Now().UnixNano(),
+// 		nonce:        nonce,
+// 		previousHash: previousHash,
+// 		transactions: []string{},
+// 	}
+
+// 	return &b
 // }
+
+func NewBlock(nonce int, previousHash [32]byte, transactions []*Transaction) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	b.transactions = transactions
+	return b
+}
 
 func (b *Block) Print() {
 	fmt.Printf("timestamp: %v\n", b.timestamp)
 	fmt.Printf("nonce: %v\n", b.nonce)
 	fmt.Printf("previousHash: %x\n", b.previousHash)
-	fmt.Printf("transactions: %v\n", b.transactions)
+	for _, t := range b.transactions {
+		t.Print()
+	}
 }
 
+// struct의 각 필드 마지막에 json으로 변경했을 때 필드 이름을 지정할 수 있다.
+// struct의 각 필드는 대문자로 시작해서 public 필드로 만들어주어야 접근이 가능하다.
+// `json:"timestamp"` 이 없으면 marshal 결과 필드이름은 Timestamp 이다.
 func (b *Block) MarshalJSON() ([]byte, error) {
 	return json.Marshal(struct {
-		Timestamp    int64    `json:"timestamp"`
-		Nonce        int      `json:"nonce"`
-		PreviousHash [32]byte `json:"previousHash"`
-		Transactions []string `json:"transactions"`
+		Timestamp    int64          `json:"timestamp"`
+		Nonce        int            `json:"nonce"`
+		PreviousHash [32]byte       `json:"previousHash"`
+		Transactions []*Transaction `json:"transactions"`
 	}{
 		Timestamp:    b.timestamp,
 		Nonce:        b.nonce,
@@ -56,18 +62,38 @@ func (b *Block) MarshalJSON() ([]byte, error) {
 }
 
 func (b *Block) Hash() [32]byte {
-	m, _ := json.Marshal(b)
+	m, _ := json.Marshal(b) // func (b *Block) MarshalJSON() 호출
+	// fmt.Println("!!!")
+	// fmt.Println(m)
+	// fmt.Println(sha256.Sum256([]byte(m)))
+	// fmt.Println("")
+
+	// 특정 Block을 json으로 변경하려고 했는데, {} 가 출력됨
+	// Block 구조체의 필드들이 private 이기 때문(lowercase)
+	// https://stackoverflow.com/questions/26327391/json-marshalstruct-returns
+	// https://stackoverflow.com/questions/21825322/why-golang-cannot-generate-json-from-struct-with-front-lowercase-character
+
+	// 따라서 별도의 marshal()을 정의: MarshalJSON
+
 	// fmt.Println(string(m))
 	return sha256.Sum256([]byte(m)) // sha256.Sum256()는 해시 값을 만드는 함수
 }
 
 type Blockchain struct {
-	transactionPool []string
+	transactionPool []*Transaction
 	chain           []*Block
 }
 
+func (bc *Blockchain) CreateBlock(nonce int, previousHash [32]byte) *Block {
+	b := NewBlock(nonce, previousHash, bc.transactionPool)
+	bc.chain = append(bc.chain, b)
+	bc.transactionPool = []*Transaction{}
+	return b
+}
+
 func NewBlockchain() *Blockchain {
-	b := &Block{}
+	// b := &Block{}
+	b := new(Block)
 	bc := new(Blockchain)
 	// bc := Blockchain{}
 	bc.CreateBlock(0, b.Hash())
@@ -79,11 +105,38 @@ func (bc *Blockchain) LastBlock() *Block {
 	return bc.chain[len(bc.chain)-1]
 }
 
-func (bc *Blockchain) CreateBlock(nonce int, previousHash [32]byte) *Block {
-	b := NewBlock(nonce, previousHash)
-	bc.chain = append(bc.chain, b)
+func (bc *Blockchain) AddTransaction(sender string, recipient string, value float32) {
+	t := newTransaction(sender, recipient, value)
+	bc.transactionPool = append(bc.transactionPool, t)
+}
 
-	return b
+type Transaction struct {
+	senderAddress    string
+	recipientAddress string
+	value            float32
+}
+
+func newTransaction(sender string, recipient string, value float32) *Transaction {
+	return &Transaction{sender, recipient, value}
+}
+
+func (t *Transaction) Print() {
+	fmt.Printf("%s\n", strings.Repeat("-", 40))
+	fmt.Printf(" Sender Address:    %v\n", t.senderAddress)
+	fmt.Printf(" Recipient Address: %v\n", t.recipientAddress)
+	fmt.Printf(" Value:             %.1f\n", t.value)
+}
+
+func (t *Transaction) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		SenderAddress    string  `json:"senderAddress"`
+		RecipientAddress string  `json:"recipientAddress"`
+		Value            float32 `json:"value"`
+	}{
+		SenderAddress:    t.senderAddress,
+		RecipientAddress: t.recipientAddress,
+		Value:            t.value,
+	})
 }
 
 func (bc *Blockchain) Print() {
@@ -98,10 +151,13 @@ func main() {
 	bc := NewBlockchain()
 	bc.Print()
 
+	bc.AddTransaction("A", "B", 1.0)
 	previousHash := bc.LastBlock().Hash()
 	bc.CreateBlock(5, previousHash)
 	bc.Print()
 
+	bc.AddTransaction("C", "D", 2.0)
+	bc.AddTransaction("X", "Y", 3.0)
 	previousHash = bc.LastBlock().Hash()
 	bc.CreateBlock(2, previousHash)
 	bc.Print()
